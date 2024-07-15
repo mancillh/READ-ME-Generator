8f6a4b801a262471259da34b3ff31368f7610736
LeonathaTurtle <mancillh@gmail.com>
2024-07-15 00:06:47 -0500

    added GitHub username, added specific linked information for the three major licenses

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -5,22 +5,24 @@
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
   if (license == "Apache License v2.0") {
-  return `[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`;
+  return `[![Apache License v2.0](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`;
   } else if (license == "MIT License") {
-  return `[![GitHub license](https://img.shields.io/github/license/Naereen/StrapDown.js.svg)](https://github.com/Naereen/StrapDown.js/blob/master/LICENSE)`;
+  return `[![MIT License](https://img.shields.io/github/license/Naereen/StrapDown.js.svg)](https://github.com/Naereen/StrapDown.js/blob/master/LICENSE)`;
   } else if (license == 'GNU General Public License v3.0') {
-    return `[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)`
+    return `[![GNU General Public License v3.0](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)`
   } else return '';
 }
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
-  if (license !== "none") {
-  return `https://opensource.org/licenses`;
-  } else {
-    return '';
-  }
+  if (license == "Apache License v2.0") {
+    return `[Apache License v2.0 Link](https://opensource.org/license/apache-2-0)`;
+    } else if (license == "MIT License") {
+    return `[MIT License Link](https://opensource.org/license/MIT)`;
+    } else if (license == 'GNU General Public License v3.0') {
+      return `[GNU General Public License v3.0 Link](https://opensource.org/license/gpl-3-0)`;
+    } else return '';
 }
 
 // TODO: Create a function that returns the license section of README
@@ -75,6 +77,7 @@ function generateMarkdown(answers) {
   ${answers.tests}
 
   ## Questions
+  My GitHub username: ${answers.username} \n
   My GitHub profile: https://github.com/${answers.username} \n
   For additional questions, contact me at: ${answers.email}`;
 }
